25573f473cddf9f7c356616ebc7e5d24e7bdf0ba
Ivan Dimov <iv_dimov@abv.bg>
2020-07-02 14:39:12 +0300

    add missed files

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Footer = () => (
+    <footer className=" fixed-bottom text-center" >
+
+        <div className="container-fluid bg-light py-3">
+        <span  className="text-muted te"> &copy; Copyright powered by DimovDev 2020</span>
+        </div>
+    </footer>
+);
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Header = () => (
+    <header className="header">
+        <div className="title">Github Job Search</div>
+    </header>
+);
+export default Header;
