9e6dad32b075cfcb880cdf9a154b10500ae52a7b
Ivan Dimov <iv_dimov@abv.bg>
2020-07-02 17:04:41 +0300

    Updates

---
+++ precache-manifest.7131afce9e3149e1e6c33a3a8be1085f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe43a9a4081c4647531a521512021a8",
+    "url": "/github_jobs_api/index.html"
+  },
+  {
+    "revision": "2991c6804da39f64f323",
+    "url": "/github_jobs_api/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "58581d2565f22d84f14c",
+    "url": "/github_jobs_api/static/css/main.42c97396.chunk.css"
+  },
+  {
+    "revision": "2991c6804da39f64f323",
+    "url": "/github_jobs_api/static/js/2.78e3d487.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/github_jobs_api/static/js/2.78e3d487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58581d2565f22d84f14c",
+    "url": "/github_jobs_api/static/js/main.f1219cb6.chunk.js"
+  },
+  {
+    "revision": "3f4a025ea4501d0ec847",
+    "url": "/github_jobs_api/static/js/runtime-main.1015e2b5.js"
+  }
+]);
